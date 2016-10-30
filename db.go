704a013fbf24f64490a0bbe6a822eb4d0ce4ab6c
Muhammad Abd al-Rahman <m.abdelra7man@hotmail.com>
2016-10-31 01:26:42 +0200

    first commit

---
+++ db.go
@@ -0,0 +1,168 @@
+package main
+
+import (
+	"bytes"
+	"encoding/gob"
+	"errors"
+	"fmt"
+	"log"
+	"os"
+	"strconv"
+
+	"github.com/boltdb/bolt"
+)
+
+var db = dbSetup()
+
+type (
+	Config struct {
+		ID   int
+		Info ConfigInfo
+	}
+
+	ConfigInfo struct {
+		URL       string
+		DBName    string
+		OdooPass  string // Odoo Master Password
+		BackupDir string
+		Version   float64
+	}
+)
+
+func dbSetup() *bolt.DB {
+	dbDir := os.Getenv("HOME") + "/.odoobup"
+	dbPath := dbDir + "/config.db"
+
+	//check if .odoobup directory not found
+	if _, err := os.Stat(dbDir); os.IsNotExist(err) {
+		err := os.Mkdir(dbDir, 0777)
+		if err != nil {
+			log.Fatalln(err)
+		}
+	}
+
+	//create and open a database
+	db, err := bolt.Open(dbPath, 0777, nil)
+	if err != nil {
+		log.Fatalln(err)
+	}
+
+	//create config bucket
+	db.Update(func(tx *bolt.Tx) error {
+		_, err := tx.CreateBucketIfNotExists([]byte("config"))
+		if err != nil {
+			log.Fatalln(err)
+		}
+		return nil
+	})
+
+	return db
+}
+
+func (c *Config) MarshalBinary() (data []byte, err error) {
+	w := new(bytes.Buffer)
+	encoder := gob.NewEncoder(w)
+	err = encoder.Encode(c.Info)
+	if err != nil {
+		return nil, err
+	}
+	return w.Bytes(), nil
+}
+
+func (c *Config) UnmarshalBinary(data []byte) error {
+	r := bytes.NewBuffer(data)
+	decoder := gob.NewDecoder(r)
+	return decoder.Decode(&c.Info)
+}
+
+func NewConfig(ci *ConfigInfo) (*Config, error) {
+	var c Config
+
+	err := db.Update(func(tx *bolt.Tx) error {
+		bkt := tx.Bucket([]byte("config"))
+
+		seq, _ := bkt.NextSequence()
+		c.ID = int(seq)
+		c.Info = *ci
+
+		encoding, err := c.MarshalBinary()
+		if err != nil {
+			return err
+		}
+
+		if err := bkt.Put(itob(c.ID), encoding); err != nil {
+			return err
+		}
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &c, nil
+}
+
+func ConfigByID(id int) (*Config, error) {
+	var config Config
+
+	err := db.View(func(tx *bolt.Tx) error {
+		v := tx.Bucket([]byte("config")).Get(itob(id))
+		if v == nil {
+			idStr := fmt.Sprint(id)
+			return errors.New("The ID " + idStr + " was not Found")
+		}
+
+		if err := config.UnmarshalBinary(v); err != nil {
+			return err
+		}
+		config.ID = id
+
+		return nil
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return &config, nil
+}
+
+func AllConfig() ([]Config, error) {
+	var config Config
+	var allConfig []Config
+
+	err := db.View(func(tx *bolt.Tx) error {
+		if err := tx.Bucket([]byte("config")).ForEach(func(k, v []byte) error {
+			config.ID, _ = strconv.Atoi(string(k))
+			if err := config.UnmarshalBinary(v); err != nil {
+				return err
+			}
+
+			allConfig = append(allConfig, config)
+
+			return nil
+
+		}); err != nil {
+			return err
+		}
+
+		return nil
+
+	})
+	if err != nil {
+		return nil, err
+	}
+
+	return allConfig, nil
+}
+
+func DeleteConfig(id int) error {
+	return db.Update(func(tx *bolt.Tx) error {
+		return tx.Bucket([]byte("config")).Delete(itob(id))
+	})
+}
+
+func itob(v int) []byte {
+	id := fmt.Sprintf("%08d", v)
+	return []byte(id)
+}
---
+++ main.go
@@ -0,0 +1,255 @@
+package main
+
+import (
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+	"net/url"
+	"os"
+	"strconv"
+	"strings"
+	"time"
+)
+
+var (
+	version = "1.0.0.beta1"
+
+	//subcommands
+	add, del, skip *flag.FlagSet
+
+	//flags
+	id string
+	//add
+	OdooURL, dbName, pass, backupDir string
+	odooVerion                       float64
+	//del
+	delID int
+)
+
+func init() {
+	//subcommands
+	flag.NewFlagSet("version", flag.ExitOnError)
+	flag.NewFlagSet("help", flag.ExitOnError)
+	flag.NewFlagSet("show", flag.ExitOnError)
+	add = flag.NewFlagSet("add", flag.ExitOnError)
+	del = flag.NewFlagSet("del", flag.ExitOnError)
+
+	//flags
+	flag.StringVar(&id, "n", "", "set id number to backup by id, example 'odoobup -n=1' or 'odoobup -n=1,2,3'.")
+
+	//add
+	add.StringVar(&OdooURL, "url", "", "set odoo url.")
+	add.StringVar(&dbName, "db_name", "", "set odoo database name.")
+	add.StringVar(&pass, "password", "", "set odoo master password.")
+	add.StringVar(&backupDir, "backup_dir", "", "set backup path directory.")
+	add.Float64Var(&odooVerion, "version", 0.0, "set odoo version.")
+	//del
+	del.IntVar(&delID, "n", 0, "set id number to delete it.")
+}
+
+func main() {
+	flag.Parse()
+
+	if len(flag.Args()) == 0 && len(allConfig()) == 0 {
+		fmt.Println("no configuration setting found. See `odoobup help`\n" +
+			"or try `odoobup add` to add new configuration setting")
+		os.Exit(1)
+	} else {
+		if id == "" && len(flag.Arg(0)) == 0 {
+			backup()
+			os.Exit(0)
+		} else {
+			if id != "" {
+				idSliceStr := strings.Split(id, ",")
+				idSlice, err := sliceAtoi(idSliceStr)
+				if err != nil {
+					log.Println(err)
+					os.Exit(1)
+				}
+
+				backup(idSlice...)
+				os.Exit(0)
+			}
+		}
+	}
+
+	if flag.Arg(0) == "version" { // version subcommand
+		fmt.Printf("odoobup version %s \n", version)
+		os.Exit(0)
+	} else if flag.Arg(0) == "help" { // help subcommand
+		helpCommand()
+	} else if flag.Arg(0) == "add" { // add subcommand
+		addCommand()
+	} else if flag.Arg(0) == "show" { // show subcommand
+		showCommand()
+	} else if flag.Arg(0) == "del" { // show subcommand
+		delCommand()
+	} else {
+		fmt.Printf("unknown subcommand `%s`, See `odoobup help`\n", flag.Arg(0))
+		os.Exit(1)
+	}
+}
+
+func helpCommand() {
+	fmt.Printf("The commands are:\n\n")
+	fmt.Printf("\t%s\t%s\n", "add", "add new configuration setting")
+	fmt.Printf("\t%s\t%s\n", "show", "show all configurations")
+	fmt.Printf("\t%s\t%s\n", "del", "delete configuration setting by id number")
+	fmt.Printf("\t%s\t%s\n", "version", "show program version number")
+	fmt.Println()
+	fmt.Println("Use \"odoobup [subcommand] -h\" for more information about a command.")
+	fmt.Println()
+}
+
+func addCommand() {
+	add.Parse(flag.Args()[1:])
+
+	if OdooURL == "" || dbName == "" || pass == "" || backupDir == "" || odooVerion == 0.0 {
+		fmt.Println("all flags required.")
+		fmt.Println("example: `odoobup add -url='http://localhost:8069' -db_name='odoo'" +
+			" -password='odoo mastre password' -backup_dir='/home/odoo/odoo_backup' -version=8.0`")
+		os.Exit(1)
+	}
+
+	if odooVerion < 8.0 {
+		log.Println("odoobup not support version", odooVerion)
+		os.Exit(1)
+	}
+
+	ci := &ConfigInfo{
+		URL:       OdooURL,
+		DBName:    dbName,
+		OdooPass:  pass,
+		BackupDir: backupDir,
+		Version:   odooVerion,
+	}
+
+	_, err := NewConfig(ci)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+
+}
+
+func showCommand() {
+	for _, value := range allConfig() {
+		fmt.Printf("{%d: {url: %s, database name: %s, backup path directory: %s, odoo version: %.1f}}\n",
+			value.ID, value.Info.URL, value.Info.DBName, value.Info.BackupDir, value.Info.Version)
+	}
+}
+
+func delCommand() {
+	del.Parse(flag.Args()[1:])
+
+	if delID == 0 {
+		fmt.Println("Please add id number to delete it using -n flag.\nexample: `odoobup delete -n=1`")
+		os.Exit(1)
+	}
+
+	err := DeleteConfig(delID)
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+}
+
+func backup(i ...int) {
+	urlPath := "/web/database/backup"
+
+	if len(i) == 0 {
+		for _, value := range allConfig() {
+			if value.Info.Version == 8.0 {
+				backupGET(urlPath, value.Info)
+			} else {
+				backupPOST(urlPath, value.Info)
+			}
+		}
+	} else {
+		for _, v := range i {
+			c, err := ConfigByID(v)
+			if err != nil {
+				log.Println(err)
+			}
+
+			if c.Info.Version == 8.0 {
+				backupGET(urlPath, c.Info)
+			} else {
+				backupPOST(urlPath, c.Info)
+			}
+		}
+	}
+}
+
+func backupGET(urlPath string, ci ConfigInfo) {
+	token := strconv.Itoa(int(time.Now().UTC().UnixNano()) / int(time.Millisecond))
+
+	address := fmt.Sprintf("%s%s?token=%s&backup_db=%s&backup_format=%s&backup_pwd=%s",
+		ci.URL, urlPath, token, ci.DBName, "zip", ci.OdooPass)
+
+	resp, err := http.Get(address)
+
+	response(resp, ci, err)
+}
+
+func backupPOST(urlPath string, ci ConfigInfo) {
+	address := fmt.Sprintf("%s%s", ci.URL, urlPath)
+
+	resp, err := http.PostForm(address,
+		url.Values{"master_pwd": {ci.OdooPass}, "name": {ci.DBName}, "backup_format": {"zip"}})
+
+	response(resp, ci, err)
+}
+
+// Processing response
+func response(resp *http.Response, ci ConfigInfo, err error) {
+	if err != nil {
+		fmt.Printf("Err: %s, database:%s... connection refused\n", ci.URL, ci.DBName)
+		return
+	}
+
+	defer resp.Body.Close()
+
+	fileName := resp.Header.Get("Content-Disposition")
+	fileName = strings.Replace(fileName, "attachment; filename*=UTF-8''", "", -1)
+
+	fmt.Printf("Get: %s, database:%s... ", ci.URL, ci.DBName)
+
+	out, err := os.Create(ci.BackupDir + "/" + fileName)
+	if err != nil {
+		fmt.Printf("\nErr: %s, database:%s... Database does not exist or Access denied.\n", ci.URL, ci.DBName)
+		return
+	}
+	defer out.Close()
+
+	_, err = io.Copy(out, resp.Body)
+	if err != nil {
+		log.Println(err)
+		return
+	}
+	fmt.Println("Done")
+}
+
+func allConfig() []Config {
+	allConfig, err := AllConfig()
+	if err != nil {
+		log.Println(err)
+		os.Exit(1)
+	}
+	return allConfig
+}
+
+//convert string slice to integer slice
+func sliceAtoi(sa []string) ([]int, error) {
+	si := make([]int, 0, len(sa))
+	for _, a := range sa {
+		i, err := strconv.Atoi(a)
+		if err != nil {
+			return si, err
+		}
+		si = append(si, i)
+	}
+	return si, nil
+}
